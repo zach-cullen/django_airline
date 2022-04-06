58a07e74376ef579308103a9441beada856f9437
Zach Cullen <zacullen@gmail.com>
2022-04-06 12:43:29 -0500

    alter flight destination and origin to reference airport model

--- flights/models.py
+++ flights/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
+class Airport(models.Model):
+  code = models.CharField(max_length=3)
+  city = models.CharField(max_length=64)
+
+  def __str__(self):
+    return f"{selfcity} ({self.code})"
+
 # Create your models here.
 class Flight(models.Model):
-  origin = models.CharField(max_length=64)
-  destination = models.CharField(max_length=64)
+  origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+  destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
   duration = models.IntegerField()
 
   def __str__(self):
     return f"{self.id}: {self.origin} to {self.destination}"
+
+
+
