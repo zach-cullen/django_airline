f4c08ece51b76225eed1ac0e9197a29dde4a780a
Zach Cullen <zacullen@gmail.com>
2022-04-08 10:13:05 -0500

    authenticate users via login form

--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
+def index(request):
+  if not request.user.is_authenticated:
+    return HttpResponseRedirect(reverse("login"))
+
+  return render(request, "users/index.html", {
+    "users": User.objects.all()
+  })
+
+def login_view(request):
+  # POST
+  if request.method == "POST":
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request, user)
+      return HttpResponseRedirect(reverse("users:index"))
+    else:
+      return render(request, "users/login.html", {
+        "message": "Invalid Credentials."
+      })
+  
+  # GET
+  return render(request, "users/login.html")
+
+def logout_view(request):
+  pass
